8f66e992ede6ba9257344a9c9a3d74f441e526dc
macdipu <c.dipu0@gmail.com>
2026-06-25 15:09:33 +0600

    update fvm version for flutter andd drive id

--- lib/services/utilities/device_info_service.dart
+++ lib/services/utilities/device_info_service.dart
@@ -3,55 +3,65 @@ import 'dart:io';
 import 'package:customer/core/domain/usecase/usecase.dart';
 import 'package:customer/core/presentation/utils/task_runner.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter_udid/flutter_udid.dart';
 
-
-/// A service to retrieve detailed information about the current device.
 class DeviceInfoService {
   DeviceInfoService._();
   static final DeviceInfoService instance = DeviceInfoService._();
 
   final DeviceInfoPlugin _deviceInfo = DeviceInfoPlugin();
 
-  /// Retrieve full device information as a Map.
-  ResultFuture<Map<String, dynamic>> getFullDeviceInfo() async {
-    return runTask(() async {
-      if (Platform.isAndroid) {
-        final androidInfo = await _deviceInfo.androidInfo;
-        return {
-          'model': androidInfo.model,
-          'manufacturer': androidInfo.manufacturer,
-          'version': androidInfo.version.release,
-          'sdkInt': androidInfo.version.sdkInt,
-          'id': androidInfo.id,
-          'isPhysicalDevice': androidInfo.isPhysicalDevice,
-        };
-      } else if (Platform.isIOS) {
-        final iosInfo = await _deviceInfo.iosInfo;
-        return {
-          'name': iosInfo.name,
-          'model': iosInfo.model,
-          'systemName': iosInfo.systemName,
-          'systemVersion': iosInfo.systemVersion,
-          'identifierForVendor': iosInfo.identifierForVendor,
-          'isPhysicalDevice': iosInfo.isPhysicalDevice,
-        };
-      } else if (Platform.isMacOS) {
-        final macInfo = await _deviceInfo.macOsInfo;
-        return {
-          'computerName': macInfo.computerName,
-          'hostName': macInfo.hostName,
-          'model': macInfo.model,
-          'osRelease': macInfo.osRelease,
-        };
-      } else if (Platform.isWindows) {
-        final winInfo = await _deviceInfo.windowsInfo;
-        return {
-          'computerName': winInfo.computerName,
-          'numberOfCores': winInfo.numberOfCores,
-          'systemMemoryInMegabytes': winInfo.systemMemoryInMegabytes,
-        };
-      }
-      return {'platform': 'unknown'};
-    });
-  }
+  Future<String>? _deviceIdFuture;
+
+  Future<String> _resolveDeviceId() =>
+      _deviceIdFuture ??= FlutterUdid.consistentUdid;
+
+  Future<String> get deviceId => _resolveDeviceId();
+
+  ResultFuture<Map<String, dynamic>> getFullDeviceInfo() => runTask(() async {
+        final deviceId = await _resolveDeviceId();
+
+        if (Platform.isAndroid) {
+          final info = await _deviceInfo.androidInfo;
+          return {
+            'deviceId': deviceId,
+            'model': info.model,
+            'manufacturer': info.manufacturer,
+            'version': info.version.release,
+            'sdkInt': info.version.sdkInt,
+            'id': info.id,
+            'isPhysicalDevice': info.isPhysicalDevice,
+          };
+        } else if (Platform.isIOS) {
+          final info = await _deviceInfo.iosInfo;
+          return {
+            'deviceId': deviceId,
+            'name': info.name,
+            'model': info.model,
+            'systemName': info.systemName,
+            'systemVersion': info.systemVersion,
+            'identifierForVendor': info.identifierForVendor,
+            'isPhysicalDevice': info.isPhysicalDevice,
+          };
+        } else if (Platform.isMacOS) {
+          final info = await _deviceInfo.macOsInfo;
+          return {
+            'deviceId': deviceId,
+            'computerName': info.computerName,
+            'hostName': info.hostName,
+            'model': info.model,
+            'osRelease': info.osRelease,
+          };
+        } else if (Platform.isWindows) {
+          final info = await _deviceInfo.windowsInfo;
+          return {
+            'deviceId': deviceId,
+            'computerName': info.computerName,
+            'numberOfCores': info.numberOfCores,
+            'systemMemoryInMegabytes': info.systemMemoryInMegabytes,
+          };
+        }
+
+        return {'deviceId': deviceId, 'platform': 'unknown'};
+      });
 }
